74a8d22933c71d431de76777be49c0b522c8bfd1
Florjan96 <florianshiqerukaj@yahoo.com>
2022-09-04 13:30:58 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+console.log(data);
+let dogList=document.querySelector('.dogs-list')
+
+function renderTheTopListOfDog(){
+    // <li class="dogs-list__button">Mr. Bonkers</li
+for (let dog of data){
+    let dogItem=document.createElement('li')
+dogItem.className='dogs-list__button'
+dogItem.textContent=dog.name
+dogList.append(dogItem)
+
+
+    }
+}
+renderTheTopListOfDog()
